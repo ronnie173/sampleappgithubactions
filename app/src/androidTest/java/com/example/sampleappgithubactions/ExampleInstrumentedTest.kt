6eec37ac8a2e14f2ce0e63a23086c32f596f7282
Jerome Raymond <jerome.raymond@cityelectricsupply.com>
2022-08-08 11:51:35 -0500

    github actions test

---
+++ app/src/androidTest/java/com/example/sampleappgithubactions/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.sampleappgithubactions
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.sampleappgithubactions", appContext.packageName)
+    }
+}
